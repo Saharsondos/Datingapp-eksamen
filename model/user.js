c60e3f3fc96332ceaf138b60c2bea6c6537f70a9
Sahar Sondos <saharsondos@Sahars-MacBook-Air.local>
2020-11-26 19:55:14 +0100

    modelklasse users

---
+++ model/user.js
@@ -0,0 +1,14 @@
+//min modelklasse for mine brugere 
+class User {
+    constructor(username, age, gender, preferredSex, email, password){
+        this.username = username;
+        this.age = age;
+        this.gender = gender;
+        this.preferredSex = preferredSex;
+        this.email = email;
+        this.password = password
+    }
+}
+
+//eksporterer User
+module.exports = User;
